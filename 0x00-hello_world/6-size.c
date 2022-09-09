8a80afa3be72f84be138e9421609ed820d101873
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-09 04:48:27 +0000

    Very intriguing experience

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %tu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %tu byte(s)\n", sizeof(intType));
-	printf("Size of a long: %tu byte(s)\n", sizeof(longType));
-	printf("Size of a long long: %tu byte(s)\n", sizeof(longlongType));
+	printf("Size of a long int: %tu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %tu byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %tu byte(s)\n", sizeof(floatType));
 	return (0);
 }
