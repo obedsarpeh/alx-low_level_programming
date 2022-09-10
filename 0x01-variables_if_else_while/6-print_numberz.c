bdd9f3d287b2360aae6aad1ab23414d44c32442e
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-10 04:13:20 +0000

    Coding is fun

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+
+	putchar(digit);
+
+	putchar('\n');
+
+	return (0);
+}
