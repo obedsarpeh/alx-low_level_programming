fc0e26f3d902d52586e5f7b95139d58c11697281
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-10 02:12:20 +0000

    Alphabet gamble

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+	{
+			putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
