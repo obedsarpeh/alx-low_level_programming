975e2f03d59b2c40ce4318d2e2d70c78b7a57042
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-15 03:10:46 +0000

    Natural numbers

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	printf("%d\n", sum_m);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Lists all the natural
+ *numbers below 1024 (excluded)
+ *that are multiples of 3 or 5.
+ *
+ *
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int n, sum_m = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+			sum_m += n;
+		printf("%d\n", sum_m);
+	}
+		return (0);
+}
