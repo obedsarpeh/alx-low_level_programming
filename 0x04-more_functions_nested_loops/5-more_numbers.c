788fe7a5059f3f179776bb6370a6a3764a530d40
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-17 00:43:33 +0000

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 {
 	int con, a;
 
-	for (con = 0; con <= 10; con++)
+	for (con = 0; con <= 9; con++)
 
 	{
 	for (a = 0; a <= 14; a++)
 	{
 		if (a > 9)
-		
+
 			_putchar((a / 10) + '0');
 
 		_putchar((a % 10) + '0');
