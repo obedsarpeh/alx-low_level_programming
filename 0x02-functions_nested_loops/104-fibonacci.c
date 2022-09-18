52f07b68d82ff7cc9b87ec930880384d4a7f0846
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-18 04:39:58 +0000

    more on fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - checks the code
+ * Return: Always 0
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int r_ead;
+	unsigned long f1, f2, sum;
+
+	sum = 0;
+	f1 = 0;
+	f2 = 1;
+	for (r_ead = 1; r_ead <= 98; r_ead++)
+	{
+		sum = f1 + f2;
+	printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (r_ead == 98)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
