5f0d3ac89b1b788340a146c55f9471d184304a69
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-20 22:30:42 +0000

    strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - prints a string
+ *
+ *@str: string
+ *
+ *
+ *
+ */
+void _puts(char *str)
+{
+	while (*str)
+	_putchar(*str++);
+
+	_putchar('\n');
+
+}
