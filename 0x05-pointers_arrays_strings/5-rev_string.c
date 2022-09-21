4314366fa2e33003ed5e8d0f9bff28fac6f43034
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-21 08:11:00 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *rev_string - reverses a string
+ *
+ *@s: string
+ *
+ *
+ *
+ */
+void rev_string(char *s)
+{
+	int i, len, tmp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+
+	}
+	_putchar('\n');
+}
