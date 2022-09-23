b6339543cbb1cf21703811dca1eb52dac8afa2c7
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-23 02:35:42 +0000

    more strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int index = 0, dest_len = 0;
 
+	while (dest[index++])
+		dest_len++;
 
-	i = 0;
-	j = 0;
-	
-	while (dest[i++])
-		j++;
-	for (i = 0; src[i]; i++)
-		dest[j] = src[i];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
-
 }
 
