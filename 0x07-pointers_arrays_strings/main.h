34b1062b4b2dfb1bd68b1cb9a4048c226e67e19b
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-09-27 01:28:04 +0000

    memory copying

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 int main(void);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif /* MAIN_H */
