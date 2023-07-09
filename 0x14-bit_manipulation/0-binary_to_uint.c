6596d0a67c3831df3d892e20f5dacf52fba415e4
obedsarpeh <obedsarpeh1@rocketmail.com>
2023-07-09 20:59:43 +0000

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,10 +18,9 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	/**
-	 * if (b[i] != '0' || b[i] != '1')
-	 *return (0);
-	 */
+	if (b[i] != '0' || b[i] != '1')
+	return (0);
+
 	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
