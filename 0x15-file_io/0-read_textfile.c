17e775c31239418410babd1d1fdc74f81ad36381
obedsarpeh <obedsarpeh1@gmail.com>
2023-11-14 03:43:57 +0000

    changes on error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 #include "main.h"
-#include <string.h>
 #include <unistd.h>
 /**
 * read_textfile- reads a text file and prints it to standard output.
 * @filename: Name of the file to be read.
 * @letters: number of letters to be read
-* Return: Always return 0.
-*
-*
-*
+* Return: Always return 0.*
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        FILE *file;
-        char ch;
+	FILE *file;
+	char ch;
 	char *buffer;
 	ssize_t bytes_read;
 	ssize_t bytes_written;
 
-        if (filename == NULL)
-        {
-                return (0);
-        }
-        file = fopen("filename", "r");
-        if (file == NULL)
-        {
-                return (0);
-        }
-        else
-        {
-                ch = fgetc(file);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	file = fopen("filename", "r");
+	if (file == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		ch = fgetc(file);
 		printf("%c", ch);
 	}
 	buffer = malloc(letters + 1);
@@ -45,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		fclose(file);
-		return 0;
+		return (0);
 	}
 	buffer[bytes_read] = '\0';
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
@@ -53,8 +49,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		fclose(file);
-		return 0;
-       	}
+		return (0);
+	}
 	free(buffer);
 	fclose(file);
 	return (bytes_read);
